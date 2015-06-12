2bee12815d526be85e660c00c5a2e038e1fd7962
Christian Carnley <ccarnley7@gmail.com>
2015-06-12 13:02:50 -0600

    probability maps for part two

--- src/n-gram.py
+++ src/n-gram.py
@@ -11,11 +11,15 @@ model = {}
 wordToTagModel = {}
 tagToWordModel = {}
 
+wordToTagProb = {}
+tagToWordProb = {}
+
 def chooseWord(wordDictionary):
     total = 0
     probabilities = {}
     for word in wordDictionary:
         total += wordDictionary.get(word)
+    for word in wordDictionary:
         probabilities[word] = (wordDictionary.get(word)/total) * 100
     guess = random.random()
     runningTotal = 0;
@@ -26,6 +30,19 @@ def chooseWord(wordDictionary):
             return word
 
     return "ERROR"
+
+def generateProp(model):
+    newModel={}
+    for key, value in model.items():
+        probabilities = {}
+        total = 0
+        for word in value:
+            total += value.get(word)
+        for word in value:
+            probabilities[word] = (value.get(word)/total) * 100
+        newModel[key] = probabilities
+    return newModel
+
 
 def getWordList(context):
     for word in infiledata.split():
@@ -45,6 +62,8 @@ def generateSenteces(context):
     print()
 
 def trainOnData():
+    global wordToTagModel
+    global tagToWordModel
     for line in trainingData.splitlines():
         context = ['']
         for wordWithTag in line.split():
@@ -59,8 +78,10 @@ def trainOnData():
             tagToWordModel[str(tag)] = tempTwo
 
             context = (context+[word])[1:]
+    wordToTagModel = generateProp(wordToTagModel)
+    tagToWordModel = generateProp(tagToWordModel)
 
-
-# getWordList([''])
-# generateSenteces([''])
+getWordList([''])
+generateSenteces([''])
 trainOnData()
+print("test")
