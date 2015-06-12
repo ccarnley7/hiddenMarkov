d3d104481daf0cdccd2c785922d1e55f0613b482
Christian Carnley <ccarnley7@gmail.com>
2015-06-12 09:53:11 -0600

    first part of the project. DONE

--- src/n-gram.py
+++ src/n-gram.py
@@ -1,5 +1,23 @@
 import random
 
+def chooseWord(wordDictionary):
+    total = 0
+    probabilities = {}
+    for word in wordDictionary:
+        total += wordDictionary.get(word)
+        probabilities[word] = (wordDictionary.get(word)/total) * 100
+    guess = random.random()
+    runningTotal = 0;
+    for word in probabilities:
+        prob = probabilities.get(word)
+        runningTotal += prob
+        if runningTotal > guess:
+            return word
+
+    return "ERROR"
+
+
+
 infilename = "2009-obama.txt"
 trainingdata = open(infilename).read()
 
@@ -10,14 +28,17 @@ model = {}
 
 for word in trainingdata.split():
     #print (word)
-    model[str(context)] = model.setdefault(str(context),[])+ [word]
+    word = word.lower()
+    temp = model.setdefault(str(context),{})
+    temp[str(word)] = temp.get(str(word), 0) + 1
+    model[str(context)] = temp
     context = (context+[word])[1:]
 
 #print(model)
 
 context = contextconst
 for i in range(100):
-    word = random.choice(model[str(context)])
+    word = chooseWord(model[str(context)])
     print(word,end=" ")
     context = (context+[word])[1:]
 
